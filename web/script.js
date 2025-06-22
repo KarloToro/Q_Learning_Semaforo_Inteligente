430d029a0bb592909f29d418d60404743a60c083
LTprograms <105554066+LTprograms@users.noreply.github.com>
2025-06-22 17:29:38 -0500

    .

--- web/script.js
+++ web/script.js
@@ -7,10 +7,18 @@ function generateCar(axis = "Y") {
     // Posición inicial para autos verticales
     if (axis === "Y") {
         carElement.style.top = "-10%";
-        carElement.style.right = "43%";
+        carElement.style.right = "45%";
+        carElement.style.animation = "moveDown 10s linear forwards"
+    } else if (axis === "-Y") {
+        carElement.style.bottom = "-10%";
+        carElement.style.left = "45%";
+        carElement.style.animation = "moveUp 10s linear forwards"
+    } else if (aixs === "X") {
+        carElement.style.left = "-10%";
+        carElement.style.bottom = "45%";
+        carElement.style.animation = "moveLeft 10s linear forwards"
     }
 
-    // Eliminar el auto después de que termine su animación
     carElement.addEventListener("animationend", () => {
         carElement.remove();
     });
@@ -21,7 +29,7 @@ function generateCar(axis = "Y") {
 // Función para generar autos en intervalos aleatorios
 function startTrafficFlow(axis = "Y") {
     function spawn() {
-        const randomDelay = Math.random() * 2000 + 100; // entre 2s y 5s
+        const randomDelay = Math.random() * 1000 + 100; // entre 2s y 5s
         generateCar(axis);
         setTimeout(spawn, randomDelay);
     }
@@ -29,3 +37,5 @@ function startTrafficFlow(axis = "Y") {
 }
 
 startTrafficFlow("Y");
+startTrafficFlow("-Y");
+startTrafficFlow("X");
